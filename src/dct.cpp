65e8be02a09313da04eb3ca310df1d16a911be71
Scott aka: Keldo <scilley@outlook.com>
2020-04-18 10:27:55 -0400

    removed dot com

--- src/dct.cpp
+++ src/dct.cpp
@@ -41,12 +41,12 @@ int main() {
 	
 	printf("\n");
     printf("**** Created for Developers.***********************************\n");
-    printf("Coded by DevWorks OSI dot Com.                                *\n");
+    printf("Coded by DevWorks OSI.                                        *\n");
     printf("Freedom in Open Source.                                       *\n");
-	printf("Please use versioning, IE: yourproject-1.1.0.                 *\n");
-	printf("The last number in your projects version is the recent build. *\n");
-	printf("Version 1.1.0 becomes 1.1.1 and so forth on each new build.   *\n");
-	printf("***************************************************************\n");
+    printf("Please use versioning, IE: yourproject-1.1.0.                 *\n");
+    printf("The last number in your projects version is the recent build. *\n");
+    printf("Version 1.1.0 becomes 1.1.1 and so forth on each new build.   *\n");
+    printf("***************************************************************\n");
 	
 	// Let's begin work
 	std::cout << "Enter your project folders name: ";
